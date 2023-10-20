3d7001e5ba372910003862d3ae70c37443f62f9d
Alex ThinkPad <alexander.camilleri1@gmail.com>
2023-10-21 00:05:00 +0200

    progressing node.js tutorial

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+    if(req.url === '/') { // If it's / then we know that's the index page
+        res.writeHead(200, { 'Content-Type': 'text/html'}); // adding a 200 response, 
+        // meaning everything's ok, and setting the content type
+        res.end('<h1>Homepage</h1>'); // can serve HTML
+    };
+});
 
-const Logger = require('./logger');
+// Port not always going to be the same, gonna run on
+// whatever our host is going to decide inside an environment variable
+const PORT = process.env.PORT || 5000;
 
-const logger = new Logger();
-
-logger.on('message', (data) => console.log('Called Listener:', data));
-
-logger.log('Hello World');
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
