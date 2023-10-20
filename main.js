465cedf7151f5854c6c3764f6796be7ae7956587
Alex ThinkPad <alexander.camilleri1@gmail.com>
2023-10-21 01:26:08 +0200

    progressing node.js tutorial

--- main.js
+++ main.js
@@ -3,11 +3,44 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') { // If it's / then we know that's the index page
-        res.writeHead(200, { 'Content-Type': 'text/html'}); // adding a 200 response, 
-        // meaning everything's ok, and setting the content type
-        res.end('<h1>Homepage</h1>'); // can serve HTML
-    };
+    // if(req.url === '/') { // If it's / then we know that's the index page
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'index.html'),
+    //         (err, content) => {
+    //             if (err) throw err;
+    //             res.writeHead(200, { 'Content-Type': 'text/html' }); // adding a 200 response, 
+    //             // meaning everything's ok, and setting the content type
+    //             res.end(content); // serving content
+    //         }
+    //     )};
+
+    // if(req.url === '/about') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'about.html'),
+    //         (err, content) => {
+    //             if (err) throw err;
+    //             res.writeHead(200, { 'Content-Type': 'text/html' });
+    //             res.end(content); // serving content
+    //         }
+    //     )};
+
+    // if(req.url === '/api/users') {
+    //     // hardcoded JSON data
+    //     const users = [
+    //         { name: 'Bob Smith', age: 40 },
+    //         { name: 'John Doe', age: 30 }
+    //     ];
+    //     // response code 200 and set the content type as JSON
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify(users)); // serve the JSON in a string format
+    // };
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    console.log(filePath);
+    res.end();
+
 });
 
 // Port not always going to be the same, gonna run on
